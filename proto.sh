0e71d05d4577df510660fd2e1119587ab22ada91
ben <ben>
2019-12-11 19:50:07 +0800

    change

--- proto.sh
+++ proto.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 protoc --go_out . cluster/internal/data.proto
+protoc -I storage/reads/datatypes/:vendor/github.com/gogo/protobuf/ --go_out=plugins=grpc:storage/reads/datatypes predicate.proto storage_common.proto
